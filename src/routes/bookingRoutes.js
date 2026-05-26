503f8aac79d5bfceb808bcb0b88eea84d291960b
akshay kumar <akshay.1904kumar@gmail.com>
2026-05-26 14:59:17 +0530

    feat: add booking routes for availability checks, creation, and retrieval

--- src/routes/bookingRoutes.js
+++ src/routes/bookingRoutes.js
@@ -1,12 +1,12 @@
 import express from 'express';
-import {checkAvailablity,createBooking,getUsersBookings} from '../controllers/bookingController.js';
+import { checkAvailablity, createBooking, getUsersBookings } from '../controllers/bookingController.js';
 import authMiddleware from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 router.use(authMiddleware);
 
-router.post('/',createBooking);
-router.get('/my',getUsersBookings);
-router.get('/check-availability',checkAvailablity);
+router.post('/', createBooking);
+router.get('/my', getUsersBookings);
+router.get('/check-availability', checkAvailablity);
 
 export default router;
